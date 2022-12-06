e768cc6f0ac5a4a8f452b6b38cb1ea9d59739ce9
Hardik Pachory <84628216+hardik-pachory@users.noreply.github.com>
2022-12-06 18:02:59 +0530

    Update Videos.js

--- src/components/Videos.js
+++ src/components/Videos.js
@@ -3,7 +3,7 @@ import Video from './Video';
 import axios from 'axios';
 const Videos = () => {
 
-  const key = "AIzaSyCCxklQZFwjHW__l646hMdulU3Hp5e7AEs";
+  const key = "AIzaSyC2EcMy--KchZdtW_XpA03XCNSX_6yJK0c";
 
   const url = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=IN&maxResults=60&key=${key}`
   // const loaded = false;
